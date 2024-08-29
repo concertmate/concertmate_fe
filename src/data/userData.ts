c89725a653e01cb4be600b057ad0b2986dd1c993
Peter Kim <peterkim.pk1@gmail.com>
2024-08-29 17:07:28 -0400

    Added friends to userData

--- src/data/userData.ts
+++ src/data/userData.ts
@@ -1,21 +1,26 @@
 interface User {
     id: number;
     email: string;
+    friends: number[]
 }
 export const user:User[] = [{
     id: 1,
-    email: 'peterkim.pk1@gmail.com'
+    email: 'peterkim.pk1@gmail.com',
+    friends: [2,3,4]
 },
 {
     id: 2,
-    email: 'konber3@gmail.com'
+    email: 'konber3@gmail.com',
+    friends: [1,3,4]
 },
 {
     id: 3,
-    email: 'kylemboomer@gmail.com'
+    email: 'kylemboomer@gmail.com',
+    friends: [1,2,4]
 },
 {
     id: 4,
-    email: 'baileyjarvis2814@gmail.com'
+    email: 'baileyjarvis2814@gmail.com',
+    friends: [1,2,3]
 }
 ]
