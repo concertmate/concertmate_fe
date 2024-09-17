3204911401a25158c521cd1e124859b1e68804b1
Peter Kim <peterkim.pk1@gmail.com>
2024-09-16 20:01:30 -0400

    Added error handling for if user submits with empty fields

--- src/Components/CreateEventForm/CreateEventForm.tsx
+++ src/Components/CreateEventForm/CreateEventForm.tsx
@@ -2,8 +2,8 @@ import {useState} from 'react'
 import { User } from '../../data/type'
 import { tmData1 } from '../../data/TicketMasterData1'
 import { data } from '../../data/userSpotifyData1'
-import postUserEvent from '../../APICall';
-
+import {postUserEvent} from '../../APICall';
+import './CreateEventForm.css'
 interface CreateEventFormProps {
     user: User,
     selectedArtistIndex: number;
@@ -12,6 +12,7 @@ interface CreateEventFormProps {
 }
 const CreateEventForm: React.FC<CreateEventFormProps> = ({selectedArtist, user, selectedArtistIndex, showOption}) => {
   const [eventName, setEventName] = useState<string>('')
+  const [formError, setFormError] = useState<boolean>(false);
   function handleSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
     const {id} = user
@@ -34,16 +35,25 @@ const CreateEventForm: React.FC<CreateEventFormProps> = ({selectedArtist, user, 
       location: `${singleShowData.location.address.streetAddress}${singleShowData.location.address.addressLocality},${singleShowData.location.address.addressRegion} ${singleShowData.location.address.postalCode}`,
       user_id: 1
     };
-    postUserEvent({id:1,userEvent: userEvent})
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => console.log(err))
+    const eventProperties = new Set(Object.values(userEvent))
+    if (eventProperties.has("")) {
+      setFormError(true)
+    }
+    else {
+      setFormError(false)
+      postUserEvent({id:1,userEvent: userEvent})
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => console.log(err))
+    }
   }
+  
   return (
     <div className='form-wrapper'>
       <form >
         <h3>Create Event</h3>
+        <p className='form-error'style={{visibility: formError? 'visible' : 'hidden'}}>Select all event properties</p>
         <label htmlFor='event-name'></label>
         <input placeholder='Event Name' id='event-name'type='text' name='event-name' onChange={(e) => setEventName(e.target.value)}/>
         <button type='submit' onClick={(e) => handleSubmit(e)}>Create Event</button>
